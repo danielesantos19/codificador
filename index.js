af50e8cd21eb3a5e70f6781f1bf3f088ef608336
danielesantos19 <daniele.c.santos19@gmail.com>
2022-12-15 16:32:32 -0300

    arrumando bugs

--- index.js
+++ index.js
@@ -1,26 +1,38 @@
-let texto = document.querySelector("#txt-msg");
-let txt2 = document.querySelector("#txt-msg2");
+const texto = document.querySelector("#txt-msg");
+const txt2 = document.querySelector("#txt-msg2");
 
-function criptografa(){
-    let textoR= texto.value;
-    let resultadoCript = textoR.replace(/e/g, "en").replace(/i/g, "in").replace(/u/g, "un").replace(/a/g, "an")
+
+function criptografa() {
+  console.log(texto.value);
+  let textoR = texto.value;
+  let resultadoCript = textoR
+    .replace(/e/g, "en")
+    .replace(/i/g, "in")
+    .replace(/u/g, "un")
+    .replace(/a/g, "an")
     .replace(/o/g, "on");
-    document.getElementById('txt-msg2').innerHTML =  resultadoCript ; 
-    return resultadoCript;
+  txt2.value = resultadoCript;
+  
 }
 
-function descriptografar(){
-    let textoR= txt2.value;
-    let resultadoDescript =  textoR.replace(/en/g, "e").replace(/in/g, "i").replace(/un/g, "u").replace(/an/g, "a")
+function descriptografar() {
+  console.log("...");
+  console.log(texto.value);
+  let textoR = texto.value.toLowerCase();
+  let resultadoDescript = textoR
+    .replace(/en/g, "e")
+    .replace(/in/g, "i")
+    .replace(/un/g, "u")
+    .replace(/an/g, "a")
     .replace(/on/g, "o");
-    document.getElementById('txt-msg2').innerHTML =  resultadoDescript  ;
-    return resultadoDescript;
+  txt2.value = resultadoDescript;
+  
 }
 
-function copiar(){
-    let copTexto = document.getElementById("txt-msg2");
-    copTexto.select();
-    copTexto.setSelectionRange(0, 99999);
-    document.execCommand('copy');
-
+function copiar() {
+  let copTexto = document.getElementById("txt-msg2");
+  copTexto.select();
+  copTexto.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+  txt2.value = "";
 }
